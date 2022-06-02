f0b4faa6c2811c78af52c09316c01b2bc2428a7d
Yexhoo (Josue Nolasco Miranda) <josue.nolasco@gmail.com>
2022-06-02 08:25:58 -0500

    common rule unit test was fixed

--- src/commons/utils/rule/index.test.ts
+++ src/commons/utils/rule/index.test.ts
@@ -3,16 +3,14 @@ import { validate } from '../../../rules/shortcode/validator'
 import { IData } from "../../../commons/interfaces/shortcode";
 
 describe('Utils:Rule', () => {
-  it('should return same data when does not exist validation errors', () => {
+  it('should return same data when does not exist validation errors', async () => {
     const input = <IData>{ url: 'https://www.google.com'}
-    const output = check(input, validate);
+    const output = await check(input, validate);
     expect(output).toMatchObject(input);
   });
 
-  it('should return error when input data is not valid', () => {
+  it('should return error when input data is not valid', async () => {
     const input = <IData>{ url: '12345'}
-    expect(() => {
-      check(input, validate)
-    }).toThrow('The url must be a valid link')
+    expect(check(input, validate)).rejects.toThrow('The url must be a valid link');
   });
 });
